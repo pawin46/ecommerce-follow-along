9ec1ebffd81ca876ccde73a4e7b73df132b6b62a
pawin46 <pawinde@gmail.com>
2025-01-28 17:10:38 +0530

    Completed The Milestone-1,2,3 & 4

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+const app = require("./app");
+const connectDatabase = require("./db/Database");
+
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting down the server for handling uncaught exception");
+});
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/.env" });
+}
+
+connectDatabase(); 
+
+// Create server
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT }`);
+});
